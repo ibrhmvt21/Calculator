999ffe2ac39f134ed3f0d0b9f235272939a18497
ibrhmvt21 <46764374+ibrhmvt21@users.noreply.github.com>
2019-01-19 16:57:02 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -21,4 +21,10 @@ public class Calculator {
     public static double divide (double num1, double num2({
         return num1/num2;
     }
+      
+  public static double divide2 (double num1, double num2({
+        return num1/num2;
+    }                                                        
+  
+                                                          
 }
