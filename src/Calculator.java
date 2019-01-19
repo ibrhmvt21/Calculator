7f84a612bdba92bee2563dd11cf5e5034adbe734
ibrhmvt21 <46764374+ibrhmvt21@users.noreply.github.com>
2019-01-19 13:03:39 -0500

    Add multiplication method
    Calculator can multiply now.

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,4 +8,8 @@ public class Calculator {
     public static double substract (double num1, double num2){
         return num1- num2;
     }
+    
+    public static double multiplication (double num1, double num2){
+        return num1*num2;
+    }
 }
